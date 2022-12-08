c0a26854fb3d6899ba4ee74464a43a297897b04f
Jordan Colubiale <jordancolubiale@Jordans-Air-2.attlocal.net>
2022-12-08 10:09:11 -0500

    added array of days and months

---
+++ script.js
@@ -0,0 +1,3 @@
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", 
+                "July", "August", "September", "October", "November", "December"];
